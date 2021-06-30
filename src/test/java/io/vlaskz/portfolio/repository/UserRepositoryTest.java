f2c002e86bbc4a6c567f94dc0850facd905e446f
Vlaskz <sam.velasquez@hotmail.com>
2021-06-30 03:06:02 -0300

    Last Commit of the night: JPA tests has been created.

--- src/test/java/io/vlaskz/portfolio/repository/UserRepositoryTest.java
+++ src/test/java/io/vlaskz/portfolio/repository/UserRepositoryTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 @DataJpaTest
 @DisplayName("UserRepository Tests")
 class UserRepositoryTest {
@@ -16,11 +19,83 @@ class UserRepositoryTest {
     @Test
     @DisplayName("Saves User when successful")
     void savePersistUserWhenSuccessful() {
+
         User user = createUser();
+
         User savedUser = this.userRepository.save(user);
+
+
         Assertions.assertThat(savedUser).isNotNull();
+
         Assertions.assertThat(savedUser.getId()).isNotNull();
+
         Assertions.assertThat(savedUser.getName()).isEqualTo(user.getName());
+
+    }
+
+    @Test
+    @DisplayName("Update User when successful")
+    void saveUpdateUserWhenSuccessful() {
+
+        User user = createUser();
+
+        User savedUser = this.userRepository.save(user);
+
+        savedUser.setName("Paarthurnax Velasquez");
+
+        User updatedUser = this.userRepository.save(savedUser);
+
+
+        Assertions.assertThat(updatedUser).isNotNull();
+
+        Assertions.assertThat(updatedUser.getId()).isNotNull();
+
+        Assertions.assertThat(updatedUser.getName()).isEqualTo(savedUser.getName());
+
+    }
+
+    @Test
+    @DisplayName("Delete User when successful")
+    void saveDeleteUserWhenSuccessful() {
+
+        User user = createUser();
+
+        User savedUser = this.userRepository.save(user);
+
+        this.userRepository.delete(savedUser);
+
+        Optional<User> userDeleted = this.userRepository.findById(savedUser.getId());
+
+        Assertions.assertThat(userDeleted).isEmpty();
+
+    }
+
+    @Test
+    @DisplayName("Returns List<User> when successful")
+    void findByNameUserWhenSuccessful() {
+
+        User user = createUser();
+
+        User savedUser = this.userRepository.save(user);
+
+        savedUser.setName("Paarthurnax Velasquez");
+
+        List<User> users = this.userRepository.findByName(savedUser.getName());
+
+        Assertions.assertThat(users).isNotEmpty();
+        Assertions.assertThat(users).contains(savedUser);
+
+
+    }
+
+    @Test
+    @DisplayName("Returns empty list when no user's found")
+    void returnsEmptyListWhenUserIsNotFound() {
+
+
+        List<User> users = this.userRepository.findByName("no user included.");
+
+        Assertions.assertThat(users).isEmpty(cccccc);
 
 
 
