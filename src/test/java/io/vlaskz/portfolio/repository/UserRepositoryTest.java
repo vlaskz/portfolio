8950b1fd0e7a3652bc3808f4b1c1b20aee9e76f2
Vlaskz <sam.velasquez@hotmail.com>
2021-06-30 02:38:56 -0300

    Created first JPA unit test

--- src/test/java/io/vlaskz/portfolio/repository/UserRepositoryTest.java
+++ src/test/java/io/vlaskz/portfolio/repository/UserRepositoryTest.java
@@ -1,10 +1,35 @@
 package io.vlaskz.portfolio.repository;
 
+import io.vlaskz.portfolio.model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
+@DisplayName("UserRepository Tests")
 class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
 
+    @Test
+    @DisplayName("Saves User when successful")
+    void savePersistUserWhenSuccessful() {
+        User user = createUser();
+        User savedUser = this.userRepository.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isNotNull();
+        Assertions.assertThat(savedUser.getName()).isEqualTo(user.getName());
+
+
+
+    }
+
+    private User createUser(){
+        return User.builder()
+                .name("Skyrim Velasquez")
+                .email("fusrodah@icloud.com")
+                .build();
+    }
 }
